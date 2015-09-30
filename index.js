6927fc1cff773905e1364b7247427acab79960be
maximobelen <maximo@jam3.com>
2015-09-30 13:39:54 -0300

    add new ease functions

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 module.exports = ScrollManager;
 
-function ScrollManager() {
-  if (!(this instanceof ScrollManager)) 
+function ScrollManager () {
+  'use strict';
+  if (!(this instanceof ScrollManager)){
     return new ScrollManager();
+  }
 }
 
-ScrollManager.prototype.scrollTop = function(options, callback) {
-
+ScrollManager.prototype.scrollTop = function (options, callback) {
+    'use strict';
     var element = options.element,
     duration = options.duration,
     ease = options.ease;
@@ -15,8 +17,8 @@ ScrollManager.prototype.scrollTop = function(options, callback) {
 
 };
 
-ScrollManager.prototype.scrollBottom = function(options, callback) {
-
+ScrollManager.prototype.scrollBottom = function (options, callback) {
+    'use strict';
     var element = options.element,
     duration = options.duration,
     ease = options.ease;
@@ -27,21 +29,8 @@ ScrollManager.prototype.scrollBottom = function(options, callback) {
 
 };
 
-ScrollManager.prototype.scrollEqual = function(options, callback) {
-
-    var element = options.element,
-    velocity = options.velocity,
-    to = options.to,
-    ease = options.ease;
-
-    var duration = to/velocity;
-
-    this.scrollTo({element: element, duration: duration, to: to, ease: ease}, callback);
-
-};
-
-ScrollManager.prototype.scrollTo = function(options, callback) {
-
+ScrollManager.prototype.scrollTo = function (options, callback) {
+    'use strict';
     var element = options.element,
     to = options.to,
     duration = options.duration,
@@ -51,14 +40,15 @@ ScrollManager.prototype.scrollTo = function(options, callback) {
     change = to - start;
     var startTime = new Date();
 
-    if(!ease)
+    if (!ease) {
       console.warn('[scroll-manager]: No ease defined, animation is going to run with linear ease');
+    }
 
-    var animate = function(elapsedTime) {
+    var animate = function (elapsedTime) {
         var currentTime = new Date();
-        elapsedTime = (currentTime.getTime() - startTime.getTime())/1000;
+        elapsedTime = (currentTime.getTime() - startTime.getTime()) / 1000;
         var position;
-        switch(ease){
+        switch (ease) {
           case 'easeLinear':
             position = easeLinear(elapsedTime, start, change, duration); 
             break;
@@ -80,8 +70,27 @@ ScrollManager.prototype.scrollTo = function(options, callback) {
           case 'easeInOutCubic': 
             position = easeInOutCubic(elapsedTime, start, change, duration);
             break;
+          case 'easeInExpo': 
+            position = easeInExpo(elapsedTime, start, change, duration);
+            break;
+          case 'easeOutExpo': 
+            position = easeOutExpo(elapsedTime, start, change, duration);
+            break;
+          case 'easeInOutExpo': 
+            position = easeInOutExpo(elapsedTime, start, change, duration);
+            break;
+          case 'easeInCirc': 
+            position = easeInCirc(elapsedTime, start, change, duration);
+            break;
+          case 'easeOutCirc': 
+            position = easeOutCirc(elapsedTime, start, change, duration);
+            break;
+          case 'easeInOutCirc': 
+            position = easeInOutCirc(elapsedTime, start, change, duration);
+            break;
           default:
             position = easeLinear(elapsedTime, start, change, duration);
+            break;
         }
 
         element.scrollTop = position; 
@@ -89,8 +98,10 @@ ScrollManager.prototype.scrollTo = function(options, callback) {
             setTimeout(function() {
                 animate(elapsedTime);
             }, null);
-        }else{
-          if(callback) callback();
+        } else {
+          if (callback) {
+            callback();
+          } 
         }
     }.bind(this);
     
@@ -98,20 +109,24 @@ ScrollManager.prototype.scrollTo = function(options, callback) {
 };
 
 var easeLinear = function (currentTime, start, change, duration) {
+  'use strict';
   return change * currentTime/duration + start;
 };
 
 var easeInQuad = function (currentTime, start, change, duration) {
+  'use strict';
   currentTime /= duration;
   return change * currentTime * currentTime + start;
 };
 
 var easeOutQuad = function (currentTime, start, change, duration) {
+  'use strict';
   currentTime /= duration;
   return -change * currentTime * (currentTime-2) + start;
 };
 
 var easeInOutQuad = function (currentTime, start, change, duration) {
+  'use strict';
   currentTime /= duration/2;
   if (currentTime < 1) return change/2 * currentTime * currentTime + start;
   currentTime--;
@@ -119,19 +134,58 @@ var easeInOutQuad = function (currentTime, start, change, duration) {
 };
 
 var easeInCubic = function (currentTime, start, change, duration) {
+  'use strict';
   currentTime /= duration;
   return change * currentTime * currentTime * currentTime + start;
 };
 
 var easeOutCubic = function (currentTime, start, change, duration) {
-    currentTime /= duration;
-    currentTime--;
-    return change * (currentTime * currentTime * currentTime + 1) + start;
+  'use strict';
+  currentTime /= duration;
+  currentTime--;
+  return change * (currentTime * currentTime * currentTime + 1) + start;
 };
 
 var easeInOutCubic = function (currentTime, start, change, duration) {
+  'use strict';
   currentTime /= duration/2;
   if (currentTime < 1) return change/2 * currentTime * currentTime * currentTime + start;
   currentTime -= 2;
   return change/2 * (currentTime * currentTime * currentTime + 2) + start;
+};
+
+var easeInExpo = function (currentTime, start, change, duration){
+  'use strict';
+  return change * Math.pow( 2, 10 * (currentTime/duration - 1) ) + start;
+};
+
+var easeOutExpo = function (currentTime, start, change, duration) {
+  'use strict';
+  return change * ( -Math.pow( 2, -10 * currentTime/duration ) + 1 ) + start;
+};
+
+var easeInOutExpo = function (currentTime, start, change, duration) {
+  'use strict';
+  currentTime /= duration/2;
+  if (currentTime < 1) return change / 2 * Math.pow( 2, 10 * (currentTime - 1) ) + start;
+  currentTime--;
+  return change/2 * ( -Math.pow( 2, -10 * currentTime) + 2 ) + start;
+};
+
+var easeInCirc = function (currentTime, start, change, duration) {
+  currentTime /= duration;
+  return -change * (Math.sqrt(1 - currentTime * currentTime) - 1) + start;
+};
+
+var easeOutCirc = function (currentTime, start, change, duration) {
+  currentTime /= duration;
+  currentTime--;
+  return change * Math.sqrt(1 - currentTime * currentTime) + start;
+};
+
+var easeInOutCirc = function (currentTime, start, change, duration) {
+  currentTime /= duration/2;
+  if (currentTime < 1) return -change/2 * (Math.sqrt(1 - currentTime * currentTime) - 1) + start;
+  currentTime -= 2;
+  return change/2 * (Math.sqrt(1 - currentTime * currentTime) + 1) + start;
 };
